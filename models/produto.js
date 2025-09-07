628c0e6557918efe1f982865dc089e9b7c847a04
VitoriaSoares20 <vitoria.lorrayne@sga.pucminas.br>
2025-09-06 23:14:05 -0300

    updates

--- models/produto.js
+++ models/produto.js
@@ -3,7 +3,10 @@ const mongoose = require('mongoose');
 const produtoSchema = mongoose.Schema({
   nome: String,
   imagem: String,
-  contagemEstoque: Number,
+  contagemEstoque: {
+    type: Number,
+    required: true,
+  },
 });
 
 exports.Produto = mongoose.model('Produto', produtoSchema);
