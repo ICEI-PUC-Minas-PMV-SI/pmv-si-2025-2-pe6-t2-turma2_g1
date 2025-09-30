56e8cab4c82d59d70b9f70b2347dfeab1d335717
Helberth A. D. Martins <helberthadm@gmail.com>
2025-09-29 22:25:14 -0300

    Desenvolvimento de Models Produtos.V e Desenvolvimento de Routers Produtos.V

--- routers/produtos.js
+++ routers/produtos.js
@@ -1,10 +1,16 @@
-const { Categoria } = require('../models/categoria');
-const { Produto } = require('../models/produto');
-const express = require('express');
+const { Categoria } = require("../models/categoria");
+const { Produto } = require("../models/produto");
+const express = require("express");
 const router = express.Router();
+const mongoose = require("mongoose");
 
+//rota get com filtragem por categorias ex: http://localhost:3000/api/v1/produtos?categorias=68d5c2b45a9ca4a5ed0b7c16
 router.get(`/`, async (req, res) => {
-  const produtoLista = await Produto.find();
+  let filtro = {};
+  if (req.query.categorias) {
+    filtro = { categoria: req.query.categorias.split(",") };
+  }
+  const produtoLista = await Produto.find(filtro).populate("categoria");
 
   if (!produtoLista) {
     res.status(500).json({ success: false });
@@ -12,11 +18,21 @@ router.get(`/`, async (req, res) => {
   res.send(produtoLista);
 });
 
+// solicitacao get apenas para listar um produto
+router.get(`/:id`, async (req, res) => {
+  const produto = await Produto.findById(req.params.id).populate("categoria");
+
+  if (!produto) {
+    res.status(500).json({ success: false });
+  }
+  res.send(produto);
+});
+
 router.post(`/`, async (req, res) => {
   const categoria = await Categoria.findById(req.body.categoria);
-  if (!categoria) return res.status(400).send('Categoria invalida');
+  if (!categoria) return res.status(400).send("Categoria invalida");
 
-  const produto = new Produto({
+  let produto = new Produto({
     nome: req.body.nome,
     descricao: req.body.descricao,
     descricaoDetalhada: req.body.descricaoDetalhada,
@@ -24,11 +40,12 @@ router.post(`/`, async (req, res) => {
     marca: req.body.marca,
     preco: req.body.preco,
     categoria: req.body.categoria,
-    contagemEstoque: req.body.contagemEstoque, //35:00
+    contagemEstoque: req.body.contagemEstoque,
+    emDestaque: req.body.emDestaque,
   });
   produto = await produto.save();
 
-  if (!produto) return res.status(500).send('Produto não pode ser criado');
+  if (!produto) return res.status(500).send("Produto não pode ser criado");
 
   res.send(produto);
 });
